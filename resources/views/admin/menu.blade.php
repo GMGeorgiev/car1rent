90fd2ec1c5d182a1907831a16e75939c162191b4
GMGeorgiev <georgemartingeorgiev@yahoo.com>
2020-06-26 01:08:14 +0300

    added EventHelper

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -8,7 +8,7 @@
             <i class="fa fa-bar-chart"></i> <span>{{  __('language.dashboard')}}</span>
         </a>
         <a class="{{ Request::is('admin/users') ? ' active' : '' }}" href="{{ route('admin.users') }}">
-            <i class="fa fa-group"></i> <span>{{  __('language.bookingInfo')}}</span>
+            <i class="fa fa-group"></i>Booking Info</span>
         </a>
         <a class="{{ Request::is('admin/users') ? ' active' : '' }}" href="{{ route('admin.users') }}">
             <i class="fa fa-group"></i> <span>{{  __('language.users')}}</span>
